8222df7c4bbaaa0b969d4046028f5825cc48980d
aleksasjade <aleksasjadevicius@gmail.com>
2022-01-11 15:41:17 +0200

    Paskaitos

---
+++ Paskaita 3/Paskaita 3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paskaita_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Iveskite tris skaicius: ");
+            //int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            //int skaicius2 = Convert.ToInt32(Console.ReadLine());
+            //int skaicius3 = Convert.ToInt32(Console.ReadLine());
+
+            //switch (skaicius1)
+            //{
+            //    case 1:
+            //        Console.WriteLine($"Visu skaiciu suma {skaicius1 + skaicius2 + skaicius3}");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine($"Pirmo ir trecio skaiciu skirtumas {skaicius1 - skaicius3}");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine($"Antro ir trecio skaiciu sandauga {skaicius2 * skaicius3}");
+            //        break;
+            //}
+            //Console.ReadKey();
+
+            /////////////////////////////////////////////////////////////////////
+
+            //Console.WriteLine("Pasirinkite gerima:\n 1 - Limonadas\n 2 - Arbata\n 3 - Kakava\n 4 - Kava\n 5 - Nieko");
+            //Console.Write("Iveskite pasirinkimo numeri: ");
+            //int pasirinkimas = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("");
+
+            //switch (pasirinkimas)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Jus pasirinkote limonada");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Jus pasirinkote arbata");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Jus pasirinkote kakava");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Jus pasirinkote kava");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Jus pasirinkote nieko");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Netinkamas pasirinkimas");
+            //        break;
+            //}
+            //Console.ReadKey();
+
+        }
+    }
+}
